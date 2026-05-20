d28c8a4537437c7dd85a2a171371aa8f1c57063d
$tr!k0d3r <ameen.yaseen.98@gmail.com>
2026-05-20 13:58:48 +0200

    Refactor fuzzing script to remove echo statements

--- Scripts/commands/fuzzing.sh
+++ Scripts/commands/fuzzing.sh
@@ -1,42 +1,37 @@
 #!/bin/bash
 
-echo "(hint) run nikto or nuclei if stuck:"
-echo "nikto -h \$IP -p 433,1433,80"
-echo "nuclei -target http://\$IP"
-echo ""
-echo "========================================"
-echo ""
+cat << 'EOF'
 
-# Directory fuzzing with different wordlists
-echo "ffuf -u http://\$IP/FUZZ -w \$raft_dir -t 300"
-echo "ffuf -u http://\$IP/FUZZ -w \$dirb -t 300"
-echo "ffuf -u http://\$IP/FUZZ -w \$dir_list -t 300"
-echo ""
+#don't forget to read the source code
+nikto -h $IP -p 433,1433,80
+nuclei -fr -target http://$IP
+nuclei -fr -target http://$IP -headless
+wapplyzer
+========================================
 
-# File fuzzing with PHP extension
-echo "ffuf -u http://\$IP:5002/FUZZ -w \$raft_files -e .php"
-echo ""
+ffuf -u http://$IP/FUZZ -w $raft_dir -t 300
+ffuf -u http://$IP/FUZZ -w $dirb -t 300
+ffuf -u http://$IP/FUZZ -w $dir_list -t 300
+/opt/SecLists/Discovery/Web-Content/spring-boot.txt 
 
-# API endpoint fuzzing
-echo "ffuf -u http://\$IP:5002/FUZZ -w /usr/share/wordlists/seclists/Discovery/Web-Content/api/api-endpoints-res.txt"
-echo ""
+ffuf -u http://$IP:5002/FUZZ -w $raft_files -e .php
 
-# Pattern-based fuzzing
-echo "# or if we see a specific pattern: vim pattern.txt"
-echo "# {GOBUSTER}/v1"
-echo "# {GOBUSTER}/v2"
-echo ""
-echo "gobuster dir -u http://\$IP:5002 -w /usr/share/wordlists/dirb/big.txt -p pattern.txt"
-echo ""
+ffuf -u http://$IP:5002/FUZZ -w /usr/share/wordlists/seclists/Discovery/Web-Content/api/api-endpoints-res.txt
 
-# VHost fuzzing
-echo "#vhost:"
-echo "ffuf -H \"Host: FUZZ.website.com\" -H \"User-Agent: PENTEST\" -c -w \"/usr/share/seclists/Discovery/DNS/subdomains-top1million-110000.txt\" -u http://website.com"
-echo ""
+# or if we see a specific pattern: vim pattern.txt
+# {GOBUSTER}/v1
+# {GOBUSTER}/v2
 
-# DNS fuzzing placeholder
-echo "#dns"
-echo "gobuster dns -d inlanefreight.com -w /usr/share/seclists/Discovery/DNS/namelist.txt"
-echo "ffuf -w /usr/share/wordlists/seclists/Discovery/DNS/namelist.txt -H \"Host: FUZZ.acmeitsupport.thm\" -u http://10.10.227.197 -fs [size] (brute forcing sub domains)"
-echo "dnsenum <domain>"
+gobuster dir -u http://$IP:5002 -w /usr/share/wordlists/dirb/big.txt -p pattern.txt
 
+#vhost:
+ffuf -u https://$IP -H "Host: FUZZ.planning.htb" -w /usr/share/seclists/Discovery/DNS/bitquark-subdomains-top100000.txt --fc 301 -t 300
+wfuzz -u http://$IP/ -H "Host: FUZZ.oniichan.htb" -w /usr/share/seclists/Discovery/DNS/subdomains-top1million-5000.txt --hw 28 -t 50
+
+#dns
+gobuster dns -d inlanefreight.com -w /usr/share/seclists/Discovery/DNS/namelist.txt
+ffuf -w /usr/share/wordlists/seclists/Discovery/DNS/namelist.txt -H "Host: FUZZ.acmeitsupport.thm" -u http://10.10.227.197 -fs [size] (brute forcing sub domains)
+dnsenum <domain>
+noauth_dns $IP
+
+EOF
